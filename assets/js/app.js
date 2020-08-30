d3785088d07cf01e8265615381622aa021e9ab21
JosueDLA <josuedlavs@gmail.com>
2020-08-30 09:29:39 -0600

    Added auto load on readme textarea

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,38 @@
+/* Markdown Render */
+window.onload = function () {
+    let md = window.markdownit();
+    loadFile('./docs/Basic-readme-en.md', md);
+
+    document.querySelector('#markdown').addEventListener('keyup', function (e) {
+        output.innerHTML = md.render(e.target.value);
+    });
+}
+
+function loadFile(file, md) {
+    let raw = new XMLHttpRequest();
+    raw.open('GET', file, true);
+    raw.send();
+
+    raw.onreadystatechange = function () {
+        if (this.status == 200 && this.readyState == 4) {
+            let text = raw.responseText;
+
+            let textArea = document.querySelector('#markdown');
+            let output = document.querySelector('#output');
+
+            //Markdown-it Render
+            textArea.innerHTML = text;
+            output.innerHTML = md.render(text);
+
+            autosize(document.querySelector('#markdown'));
+        }
+    };
+}
+
+/* Carousel */
 $('#templates').carousel({
     interval: 10000
-})
+});
 
 $('.carousel .carousel-item').each(function () {
     var minPerSlide = 3;
@@ -19,14 +51,3 @@ $('.carousel .carousel-item').each(function () {
         next.children(':first-child').clone().appendTo($(this));
     }
 });
-
-//Markdown-it Render
-var md = window.markdownit();
-var output = document.querySelector('#output');
-output.innerHTML = md.render(document.querySelector('#markdown').textContent);
-
-document.querySelector('#markdown').addEventListener('keyup', function (e) {
-    output.innerHTML = md.render(e.target.value);
-});
-
-autosize(document.querySelector('#markdown'));
---
+++ docs/Basic-readme-es.md
@@ -0,0 +1,54 @@
+# Nombre del proyecto
+
+Breve descripción del proyecto.
+
+## Instalación
+
+Lista las instrucciones de como obtener el proyecto y correrlo en su maquina local.
+
+### Pre-requisitos
+
+_Una guía de como instalar las herramientas necesarias para correr el proyecto._
+
+Explica el proceso paso a paso.
+
+```bash
+Install something
+```
+
+## Como usarlo
+
+Explica como probar el proyecto y brinda algún ejemplo.
+
+```bash
+Example
+```
+
+## Deploy
+
+Describe las herramientas necesarias para desplegar el proyecto.
+
+## Tecnologías
+
+_Nombra las tecnologias utiliadas en el proyecto._ 
+* [Spring](https://spring.io/) - Framework Used.
+* [React](https://reactjs.org/) - UI Library.
+* [Hibernate](https://hibernate.org/) - ORM.
+
+## Contribuciones
+
+Describe los pasos a seguir si alguien desea contribuir a tu proyecto.
+
+## Documentación
+
+Especifica [donde](https://es.wikipedia.org/wiki/Wikipedia:Portada) se puede encontrar mas documentación acerca de tu proyecto.
+
+## Reconocimientos
+
+_Menciona a las personas que te ayudaron a desarrollar el proyecto, te inspiraron etc._
+
+* [Linus Torvalds](https://github.com/torvalds)
+* [Dan Abramov](https://github.com/gaearon)
+
+## Licencia
+Describe los acuerdos de [licencia](https://choosealicense.com/) del proyecto.
